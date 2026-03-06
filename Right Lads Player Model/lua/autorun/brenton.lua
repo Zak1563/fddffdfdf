490ff9db860854bd83026d0abbe61852813b3b9e
Zak1563 <prisepamaksim02@gmail.com>
2026-03-06 14:53:56 +0300

    Add files via upload

---
+++ Right Lads Player Model/lua/autorun/brenton.lua
@@ -0,0 +1,3 @@
+player_manager.AddValidModel( "Brenton", "models/player/brenton/ron_fbi/models/brenton.mdl" );
+player_manager.AddValidHands( "Brenton", "models/player/brenton/hands/ron_fbi_arms/models/brentonhands.mdl", 0, "00000000" )
+list.Set( "PlayerOptionsModel", "Brenton", "models/player/brenton/brenton.mdl" );
